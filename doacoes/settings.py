99efecbe1958cae4a79639ee19eb4d9cb3b8f116
André Luiz <contato@xdvl.info>
2016-04-06 01:45:05 -0300

    template integrado com o django

--- doacoes/settings.py
+++ doacoes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 from pathlib import Path
+from uuid import UUID
 
 from django.utils import crypto
 
@@ -234,4 +235,4 @@ LOGGING = {
 # http://docs.mundipagg.com/
 
 MUNDIPAGG_API_ENDPOINT = config('MUNDIPAGG_API_ENDPOINT', default='https://sandbox.mundipaggone.com/')
-MUNDIPAGG_API_KEY = config('MUNDIPAGG_API_KEY', default='076550d2-b27e-4d88-8bda-a5115f6591d6')
+MUNDIPAGG_API_KEY = config('MUNDIPAGG_API_KEY', default='076550d2-b27e-4d88-8bda-a5115f6591d6', cast=UUID)
--- doacoes/urls.py
+++ doacoes/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', admin.site.urls),
 ]
