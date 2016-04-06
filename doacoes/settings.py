211b67a93e6caf91fb3825ea07301af7ecda7fb4
André Luiz <contato@xdvl.info>
2016-04-06 02:33:28 -0300

    simplifica o settings

--- doacoes/settings.py
+++ doacoes/settings.py
@@ -151,20 +151,7 @@ AWS_PRELOAD_METADATA = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE', default='django.core.files.storage.FileSystemStorage')
-DEFAULT_S3_PATH = 'media'
-
-STATICFILES_STORAGE = config('STATICFILES_STORAGE', default='django.contrib.staticfiles.storage.StaticFilesStorage')
-STATIC_S3_PATH = 'static'
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-]
-
 STATICFILES_DIRS = [
-    # str(BASE_DIR / 'bower_components'),
     str(BASE_DIR / 'doacoes' / 'static'),
 ]
 
