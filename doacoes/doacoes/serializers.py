e2d20e23631a4d5f06cd30b5c7ebed49ada3a0d2
André Luiz <contato@xdvl.info>
2016-04-12 04:01:49 -0300

    move as bandeiras para o serializer

--- doacoes/doacoes/serializers.py
+++ doacoes/doacoes/serializers.py
@@ -2,10 +2,32 @@ from django.utils import timezone
 
 from rest_framework import serializers
 
-from .models import Bandeira
-
 
 class DoacaoSerializer(serializers.Serializer):
+    VISA = "Visa"
+    MASTERCARD = "Mastercard"
+    HIPERCARD = "Hipercard"
+    AMEX = "Amex"
+    DINERS = "Diners"
+    ELO = "Elo"
+    AURA = "Aura"
+    DISCOVER = "Discover"
+    CASASHOW = "CasaShow"
+    HUGCARD = "HugCard"
+
+    BANDEIRA_CHOICES = (
+        (VISA, "Visa"),
+        (MASTERCARD, "Mastercard"),
+        (HIPERCARD, "Hipercard"),
+        (AMEX, "Amex"),
+        (DINERS, "Diners"),
+        (ELO, "Elo"),
+        (AURA, "Aura"),
+        (DISCOVER, "Discover"),
+        (CASASHOW, "CasaShow"),
+        (HUGCARD, "HugCard"),
+    )
+
     email = serializers.EmailField()
 
     numero_cartao = serializers.CharField()
@@ -13,10 +35,10 @@ class DoacaoSerializer(serializers.Serializer):
     validade_mes = serializers.IntegerField()
     validade_ano = serializers.IntegerField()
     verificador = serializers.IntegerField()
-    bandeira_cartao = serializers.ChoiceField(Bandeira.choices())
+    bandeira_cartao = serializers.ChoiceField(BANDEIRA_CHOICES)
 
     valor = serializers.IntegerField()
-    recorrencia = serializers.BooleanField()
+    recorrencia = serializers.BooleanField(default=False)
 
     def validate_validade_mes(self, value):
         if value < 1 or value > 12:
