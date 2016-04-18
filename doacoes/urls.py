3e5a4e3c6b5870da3853619682532b23fbe5b9ee
André Luiz <contato@xdvl.info>
2016-04-17 21:49:52 -0300

    remove drf

--- doacoes/urls.py
+++ doacoes/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from . import api
-
 urlpatterns = [
     url(r'', include('doacoes.doacoes.urls', namespace='doacoes')),
-    url(r'^api/', include(api.router.urls)),
     url(r'^admin/', admin.site.urls),
 ]
